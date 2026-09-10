cc1731e49f487bcf795f27f6e8963a7c834561bd
Gabriel de Almeida Spadafora <84877522+GabrielSpdf@users.noreply.github.com>
2026-09-10 19:18:16 -0300

    Feat/payment (#6)
    * todo: implement payment part
    
    * feat: implement payment service (random approval)

--- assessment01/RabbitMQ_Ecommerce/microservices/ms-pagamento/service.go
+++ assessment01/RabbitMQ_Ecommerce/microservices/ms-pagamento/service.go
@@ -1,9 +1,13 @@
 package mspagamento
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand"
 
 	"RabbitMQ_Ecommerce/utils/events"
+	"RabbitMQ_Ecommerce/utils/misc"
 	"RabbitMQ_Ecommerce/utils/rabbitmq"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -50,8 +54,6 @@ func InitMSPagamento() (
 	}
 	log.Println("[SUCESSO] Fila pagamento declarada")
 
-	// pedido.excluido pode ser adicionado aqui caso o enunciado exija que o
-	// Pagamento também trate cancelamentos de pedidos ainda não processados.
 	for _, routingKey := range []string{
 		events.PedidoEstoqueOk,
 	} {
@@ -75,4 +77,134 @@ func InitMSPagamento() (
 	}
 
 	return runTime, nil
+}
+
+func DecideApproval() bool {
+	/*
+		True = Pagamento aprovado (70%)
+		False = Pagamento recusado (30%)
+	*/
+	return rand.Float64() > 0.3
+}
+
+func HandlePaymentEvent(
+	envelope events.EventEnvelope,
+	channel *amqp.Channel,
+) error {
+	var payload events.OrderReferencePayload
+
+	if err := json.Unmarshal(envelope.Payload, &payload); err != nil {
+		return fmt.Errorf(
+			"erro ao desserializar pedido.estoque_ok: %w",
+			err,
+		)
+	}
+
+	switch envelope.EventType {
+	case events.PedidoEstoqueOk:
+		if DecideApproval() {
+			err := PublishPaymentOk(
+				channel,
+				payload.OrderID,
+			)
+
+			if err != nil {
+				return fmt.Errorf(
+					"erro ao processar pagamento: %w",
+					err,
+				)
+			}
+
+			log.Printf(
+				"[SUCESSO] Pagamento do pedido %s realizado com sucesso!",
+				payload.OrderID,
+			)
+		} else {
+			if err := PublishPaymentNok(
+				channel,
+				payload.OrderID,
+				"pagamento recusado pela operadora",
+			); err != nil {
+				return fmt.Errorf(
+					"erro ao processar pagamento: %w",
+					err,
+				)
+			}
+
+			log.Printf(
+				"[ERRO] Pagamento do pedido %s recusado...",
+				payload.OrderID,
+			)
+		}
+
+	default:
+		return fmt.Errorf(
+			"tipo de evento inesperado no Pagamento: %s",
+			envelope.EventType,
+		)
+	}
+
+	return nil
+}
+
+func PublishPaymentOk(
+	channel *amqp.Channel,
+	orderId string,
+) error {
+	payload := events.PaymentResultPayload{
+		OrderID: orderId,
+	}
+
+	envelope, err := misc.MountEnvelope(
+		payload,
+		events.PagamentoAprovado,
+		"ms-pagamento",
+		"signature",
+	)
+	if err != nil {
+		return fmt.Errorf("erro ao montar envelope: %w", err)
+	}
+
+	if err := rabbitmq.PublishEvent(
+		channel,
+		events.ExchangeEcommerce,
+		events.PagamentoAprovado,
+		envelope,
+	); err != nil {
+		return fmt.Errorf("erro ao enviar evento: %w", err)
+	}
+
+	return nil
+}
+
+func PublishPaymentNok(
+	channel *amqp.Channel,
+	orderId string,
+	reason string,
+) error {
+	payload := events.PaymentResultPayload{
+		OrderID: orderId,
+		Reason:  reason,
+	}
+
+	envelope, err := misc.MountEnvelope(
+		payload,
+		events.PagamentoRecusado,
+		"ms-pagamento",
+		"signature",
+	)
+	if err != nil {
+		return fmt.Errorf("erro ao montar envelope: %w", err)
+	}
+
+	if err := rabbitmq.PublishEvent(
+		channel,
+		events.ExchangeEcommerce,
+		events.PagamentoRecusado,
+		envelope,
+	); err != nil {
+		return fmt.Errorf("erro ao enviar evento: %w", err)
+	}
+
+	return nil
 }
